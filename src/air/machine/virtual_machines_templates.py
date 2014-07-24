d540b454e6dd4a254d1716e281f104738163da3f
Paweł Suder <pawel@suder.info>
2014-07-24 07:39:48 +0200

    Enable all virtual machine templates listing.

--- src/air/machine/virtual_machines_templates.py
+++ src/air/machine/virtual_machines_templates.py
@@ -5,10 +5,11 @@ __author__ = 'paoolo'
 PREFIX = '/virtual_machine_templates'
 
 
-def get_all_virtual_machines_templates():
+def get_all_virtual_machines_templates(_all=False):
     """
     Get a list of virtual machines used by appliances added to user appliance sets.
 
     :return:
     """
-    return air.tools.create_req()
+    url = '?all=true' if _all else ''
+    return air.tools.create_req(url=url)
